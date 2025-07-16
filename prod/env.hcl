a9022f1a47c85247791f2d7e4ce8cfa38f98d228
_MoonLight_ <moonlight8978@gmail.com>
2025-07-16 22:42:59 +0700

    feat(k3s): bootstrap the cluster

--- prod/env.hcl
+++ prod/env.hcl
@@ -1,3 +1,9 @@
 locals {
   debian_template_vm_id = 1000
+  kube_vip_ip4 = "192.168.0.40"
+  kube_cluster_cidr = "10.42.0.0/16"
+  kube_service_cidr = "10.43.0.0/16"
+  k3s_version  = "v1.33.1+k3s1"
+  # Set this to true to bootstrap the cluster
+  kube_bootstrap = false
 }
--- prod/kubernetes/k3sup/main.tf
+++ prod/kubernetes/k3sup/main.tf
@@ -11,35 +11,82 @@ variable "k3s_server_token" {
   type = string
 }
 
+variable "kube_vip_ip4" {
+  type = string
+}
+
+variable "kube_cluster_cidr" {
+  type = string
+}
+
+variable "kube_service_cidr" {
+  type = string
+}
+
+variable "k3s_version" {
+  type = string
+}
+
+variable "kube_bootstrap" {
+  type = bool
+}
+
 locals {
   install = var.controllers[0]
-  joins = slice(var.controllers, 1, length(var.controllers))
+  joins   = slice(var.controllers, 1, length(var.controllers))
+  tls_san = concat(var.controllers, [var.kube_vip_ip4])
 }
 
-resource "local_file" "k3sup_install_config" {
+resource "local_file" "install_config" {
   content = templatefile("${path.module}/templates/k3sup.tftpl", {
-    install = true
-    ip4 = local.install
-    name = "control-1"
-    join_url = "https://${local.install}:6443"
-    k3s_server_token = var.k3s_server_token
-    join_ip4 = local.install
+    install           = true
+    ip4               = local.install
+    name              = "control-${index(var.controllers, local.install) + 1}"
+    k3s_server_token  = var.k3s_server_token
+    join_ip4          = local.install
+    kube_cluster_cidr = var.kube_cluster_cidr
+    kube_service_cidr = var.kube_service_cidr
+    tls_san           = local.tls_san
+    k3s_version       = var.k3s_version
   })
 
-  filename = "${var.root_dir}/tmp/k3sup-1.yml"
+  filename = "${var.root_dir}/tmp/k3sup-${index(var.controllers, local.install) + 1}.yml"
 }
 
-resource "local_file" "k3sup_join_config" {
+resource "null_resource" "install" {
+  count = var.kube_bootstrap ? 1 : 0
+
+  provisioner "local-exec" {
+    command     = "c7r k3sup --config tmp/k3sup-${index(var.controllers, local.install) + 1}.yml"
+    working_dir = var.root_dir
+  }
+}
+
+resource "local_file" "join_config" {
   for_each = toset(local.joins)
 
   content = templatefile("${path.module}/templates/k3sup.tftpl", {
-    install = false
-    ip4 = each.value
-    name = "control-${index(var.controllers, each.value) + 1}"
-    join_url = "https://${local.install}:6443"
-    k3s_server_token = var.k3s_server_token
-    join_ip4 = local.install
+    install           = false
+    ip4               = each.value
+    name              = "control-${index(var.controllers, each.value) + 1}"
+    k3s_server_token  = var.k3s_server_token
+    join_ip4          = local.install
+    tls_san           = local.tls_san
+    kube_cluster_cidr = var.kube_cluster_cidr
+    kube_service_cidr = var.kube_service_cidr
+    k3s_version       = var.k3s_version
   })
 
   filename = "${var.root_dir}/tmp/k3sup-${index(var.controllers, each.value) + 1}.yml"
+}
+
+resource "null_resource" "join" {
+  for_each = var.kube_bootstrap ? toset(local.joins) : []
+
+  depends_on = [null_resource.install]
+
+  provisioner "local-exec" {
+    command     = "c7r k3sup --config tmp/k3sup-${index(var.controllers, each.value) + 1}.yml"
+    working_dir = var.root_dir
+  }
 }
