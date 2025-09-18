b9ed2d878af043b0d767f3471b253ea77875fb36
_MoonLight_ <moonlight8978@gmail.com>
2025-09-18 13:22:08 +0700

    refactor: reduce external secret manual mapping

--- prod/bitwarden/main.tf
+++ prod/bitwarden/main.tf
@@ -8,12 +8,14 @@ data "bitwarden_project" "main" {
 
 variable "secrets" {
   type = map(object({
-    data = map(string)
+    data = optional(map(string))
+    children = optional(map(map(string)))
   }))
 }
 
 locals {
-  secrets = flatten([for app, secret in var.secrets : [for key, value in secret.data : { key = "${app}/${key}", value = value }]])
+  secrets = flatten([for app, secret in var.secrets : [for key, value in secret.data : { key = "${app}/${key}", value = value }] if secret.data != null ])
+  childrens = flatten([for app, secret in var.secrets : [for child, value in secret.children : { key = "${app}/${child}", value = value }] if secret.children != null ])
 }
 
 resource "bitwarden_secret" "main" {
@@ -22,5 +24,14 @@ resource "bitwarden_secret" "main" {
   project_id = data.bitwarden_project.main.id
   key        = each.key
   value      = each.value
+  note       = "Managed by Terraform"
+}
+
+resource "bitwarden_secret" "children" {
+  for_each = { for secret in local.childrens : secret.key => secret.value }
+
+  project_id = data.bitwarden_project.main.id
+  key        = each.key
+  value      = jsonencode(each.value)
   note       = "Managed by Terraform"
 }
