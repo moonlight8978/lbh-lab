87d0cc54d8149549ccac2fb514aea82aaf6386df
Lê Sĩ Bích <moonlight8978@gmail.com>
2025-11-03 01:01:01 +0700

    feat: use zitadel for identity management (#227)

---
+++ scripts/protect-zitadel-console.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+secrets=$(sops decrypt secrets.yml | yq -r '.secrets.zitadel.children.misc')
+
+PROJECT_ID=$(yq -r '.defaultProjectId' <<< "$secrets")
+PAT=$(kubectl get secret -n application zitadel-iam-admin-pat -o yaml | yq -r '.data.pat' | base64 -d)
+
+curl -L -X PUT "https://zitadel-10-242-20-170.lab-local.bichls.dev/management/v1/projects/$PROJECT_ID" \
+-H 'Content-Type: application/json' \
+-H 'Accept: application/json' \
+-H "Authorization: Bearer $PAT" \
+--data-raw '{
+  "name": "ZITADEL",
+  "projectRoleAssertion": false,
+  "projectRoleCheck": false,
+  "hasProjectCheck": true,
+  "privateLabelingSetting": "PRIVATE_LABELING_SETTING_UNSPECIFIED"
+}'
